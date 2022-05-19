a014b16fb78cb308ff78c456d118485fb85a7b57
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-19 12:29:14 +0530

    [Elavarasu] LeapYear

---
+++ BasicCorePrograms/BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public void Year()
+        {
+            Console.WriteLine("Enter a Year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int maxDigit = year.ToString().Length;
+
+            if (maxDigit == 4)
+            {
+                if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+                {
+                    Console.WriteLine("Entered year" + " " + year + " " + "is LeapYear");
+                }
+                else
+                {
+                    Console.WriteLine("Entered year" + " " + year + " " + "is not a LeapYear");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid:Enter a 4-digit Number");
+            }
+        }
+    }
+}
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -6,8 +6,30 @@ namespace BasicCorePrograms
     {
         public static void Main(String[] args)
         {
-            CoinFlip getMethod = new CoinFlip();
-            getMethod.Toss();
+            Console.WriteLine("Enter a Number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    {
+                        CoinFlip getClass = new CoinFlip();
+                        getClass.Toss();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("LeapYear");
+                        LeapYear getClass=new LeapYear();
+                        getClass.Year();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter a Number between(1-2");
+                        break;
+                    }
+            }
         }
     }
 }
